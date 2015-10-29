0063ed9ec772a5d014d71bec36ca42f3c93eac87
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-10-29 16:03:48 +0000

    end of chap 26

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,21 +1,24 @@
 require 'game'
-# require 'player'
 
 describe Game do
-  # ivan = Player.new 'ivan'
-  # aaron = Player.new 'aaron'
   let(:ivan) {double(:ivan)}
   let(:aaron) {double(:aaron)}
-  subject(:game) { Game.new }
+  subject(:game) { Game.new(ivan, aaron) }
 
+
+  context 'retrive players' do
+    it '#player1' do
+      expect(game.player1).to eq ivan
+    end
+    it '#player2' do
+      expect(game.player2).to eq aaron
+    end
+  end
 
   context 'when attacking' do
-
     it 'damages the player' do
       expect(aaron).to receive (:cause_damage)
       game.attack(aaron)
     end
-
   end
-
 end
